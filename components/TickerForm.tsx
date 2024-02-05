bd707ec9fdefef6d9f803791b0e3c744671b8aa6
Varga György Márk <gyorgy.varga@shiwaforce.com>
2024-02-05 19:03:25 +0100

    basic client side implementation of async API called based ticker validation

--- components/TickerForm.tsx
+++ components/TickerForm.tsx
@@ -15,23 +15,48 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { ArrowRight } from 'lucide-react';
+import useRefinement, { RefinementCallback } from '@/hooks/use-refinement';
 
-const FormSchema = z.object({
+const tickerValidationSchema = z.object({
   ticker: z
     .string()
     .min(1, { message: 'Ticker must be at least 1 character.' })
     .max(5, { message: 'Ticker must be no more than 5 characters.' }),
 });
 
+type Ticker = z.infer<typeof tickerValidationSchema>;
+
+function checkTickerValidity(): RefinementCallback<Ticker> {
+  return async (data, { signal }) => {
+    const apiKey = process.env.NEXT_PUBLIC_POLYGON_API_KEY;
+    console.log(apiKey);
+    const response = await fetch(
+      `https://api.polygon.io/v3/reference/tickers?ticker=${data.ticker}&active=true&apiKey=${apiKey}`,
+      { signal }
+    );
+    const responseData = await response.json();
+    return responseData.results && responseData.results.length > 0;
+  };
+}
+
 export function TickerForm() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
+  const isTickerValid = useRefinement(checkTickerValidity(), {
+    debounce: 300,
+  });
+  const form = useForm<{ ticker: string }>({
+    resolver: zodResolver(
+      tickerValidationSchema.refine(isTickerValid, {
+        message: 'Ticker not found.',
+        path: ['ticker'],
+      })
+    ),
     defaultValues: {
       ticker: '',
     },
+    mode: 'all',
   });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
+  function onSubmit(data: { ticker: string }) {
     console.log(data);
   }
 
@@ -49,7 +74,14 @@ export function TickerForm() {
               <FormItem>
                 <FormLabel>Enter Ticker Symbol</FormLabel>
                 <FormControl>
-                  <Input placeholder='TICKER' {...field} />
+                  <Input
+                    placeholder='TICKER'
+                    {...field}
+                    onChange={(e) => {
+                      field.onChange(e);
+                      isTickerValid.invalidate();
+                    }}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
