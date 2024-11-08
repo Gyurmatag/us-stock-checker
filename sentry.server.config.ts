1e547fdc08706a46e66cafc9e1e50fcfaf461675
György Márk Varga <gyorgy.varga@shiwaforce.com>
2024-11-08 11:45:26 +0100

    added sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://a3d4b0d3cd216c16c6ac8392001f0539@o4508262042370048.ingest.de.sentry.io/4508262044139600',
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
